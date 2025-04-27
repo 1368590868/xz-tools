c11a7b1e8856be17f26f6eb4322e252a7b8c1d55
ranlin <mumu@irlin.cn>
2025-04-27 11:08:03 +0800

    fix 文本修改

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,32 +1,32 @@
-import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-components';
 import React from 'react';
 
 const Footer: React.FC = () => {
   return (
     <DefaultFooter
+      copyright={`${new Date().getFullYear()} 行展软件有限公司`}
       style={{
         background: 'none',
       }}
       links={[
         {
           key: '行展软件',
-          title: '行展软件',
-          href: 'https://pro.ant.design',
+          title: '行展银行交易台账',
+          href: '',
           blankTarget: true,
         },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
+        // {
+        //   key: 'github',
+        //   title: <GithubOutlined />,
+        //   href: 'https://github.com/ant-design/ant-design-pro',
+        //   blankTarget: true,
+        // },
+        // {
+        //   key: 'Ant Design',
+        //   title: 'Ant Design',
+        //   href: 'https://ant.design',
+        //   blankTarget: true,
+        // },
       ]}
     />
   );
